9d8da4478a31f809450a978fce222d516f1b1553
Darlan Souza Santos Cruz <100162578+Dansouza911@users.noreply.github.com>
2022-05-09 23:10:41 -0300

    DB UP

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const connection = new Sequelize(
-    'postgres://roupas_user:XQcrEzeFBXN98pUiaiadXou71747rUWq@dpg-c9qstrc41lsbneedgod0-a.oregon-postgres.render.com/roupas',
+    process.env.DATABASE.URL,
     {
         dialect: 'postgres',
         dialectOptions: {
